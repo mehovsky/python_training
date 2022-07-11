263e06b9a4cac2d0fd755d28103d3abc8f6027b7
Mekhovsky Sergey <mehovsky96@mail.ru>
2022-07-11 19:21:15 +0300

    Add test case test_edit_first_group

--- fixture/group.py
+++ fixture/group.py
@@ -38,3 +38,24 @@ class GroupHelper:
         # Submit group deletion
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
+
+    def edit_first_group(self, group):
+        wd = self.app.wd
+        self.open_group_page()
+        # Select checkbox first group
+        wd.find_element_by_name("selected[]").click()
+        # Submit "Edit group"
+        wd.find_element_by_name("edit").click()
+        # Fill group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # Submit group update
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
