363c4d6cd35a0ff790a8a332e7925be120a725af
Mekhovsky Sergey <mehovsky96@mail.ru>
2022-07-18 17:20:14 +0300

    if else then

--- fixture/group.py
+++ fixture/group.py
@@ -13,13 +13,13 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_group_page()
 
-    def edit_first_group(self, group):
+    def edit_first_group(self, new_group_data):
         wd = self.app.wd
         self.open_group_page()
         self.select_first_group()
         # Submit "Edit group"
         wd.find_element_by_name("edit").click()
-        self.fill_group_form(group)
+        self.fill_group_form(new_group_data)
         # Submit group update
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
@@ -32,17 +32,18 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
 
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
     def fill_group_form(self, group):
         wd = self.app.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
 
     def select_first_group(self):
         wd = self.app.wd
