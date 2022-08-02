6c05c08eabb883c2d7df4e9dc86e83b695d98ea2
Mekhovsky Sergey <mehovsky96@mail.ru>
2022-08-02 20:15:42 +0300

    Добавил проверки к тестам удаления, модификации и добавления групп

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,6 @@
+from model.group import Group
+
+
 class GroupHelper:
 
     def __init__(self, app):
@@ -57,6 +60,16 @@ class GroupHelper:
     def return_to_group_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_group_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name=text, id=id))
+        return groups
 
     def count(self):
         wd = self.app.wd
